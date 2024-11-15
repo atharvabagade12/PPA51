2139c7519e2163e2ff5fe1ab930919239c0c792a
Atharva Bagade <158483581+atharvabagade12@users.noreply.github.com>
2024-11-15 20:59:09 +0530

    Add files via upload

---
+++ Collections2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Collections2
+{
+    public static void main(String args[])
+    {
+        Stack <String>sobj = new Stack<String>();
+
+        sobj.push("C Programming");
+        sobj.push("C++ Programming");
+        sobj.push(" Java Programming");
+        sobj.push("Python Programming");
+
+        System.out.println(sobj);
+
+        String ret = sobj.pop();
+
+        System.out.println("popped element : "+ret);
+
+        System.out.println(sobj);
+    }
+}
---
+++ Static.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void Display()
+{
+    int i = 10;
+    static int j = 10;
+
+    i++;
+    j++;
+    
+    printf("Value of i : %d\n",i); // 11 11 11 11
+    printf("Value of j : %d\n",j); // 11 12 13 14
+    printf("\n");
+
+}
+
+int main()
+{
+    Display();
+    Display();
+    Display();
+    Display();
+
+    return 0;
+}
